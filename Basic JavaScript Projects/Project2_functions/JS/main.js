72e2a006c0b7d487e6e06f4b631df3877c543182
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2021-11-19 09:41:42 -0800

    Code for school
    Contains anther folder with a html and JavaScript file. There is also a image file for a different school assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,14 @@
+// FUNCTION THAT INCLUDES TWO VARIABLES
+
+function button1() {
+    var str = "this going to be green";
+    var result = str.fontcolor ("green");
+    document.getElementById ("green").innerHTML = result;
+}
+
+// A += concatenate operator
+function nowFunction() {
+    var sentence = "Hey how are";
+    sentence += " you doing this morning?";
+    document.getElementById("first").innerHTML = sentence;    
+}
