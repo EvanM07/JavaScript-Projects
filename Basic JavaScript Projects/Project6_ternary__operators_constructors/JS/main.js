61278fee267136831dcbb77cfebe55ae932b2243
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2021-11-21 20:01:50 -0800

    code for school
    this is code for school based on challenges that they provided. this commit includes JavaScript and HTML files

---
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,58 @@
+// TERNARY OPERATOR
+function Ride_function() {
+    var Height, can_ride;
+    Height = document.getElementById("Height").value;
+    can_ride = (Height >=52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = can_ride + " to ride.";
+}
+// TERNARY OPERATOR 2
+function vote_function() {
+    age = document.getElementById("age").value;
+    can_vote = (age >=18) ? "You are old enough":"You are not old enough";
+    document.getElementById("vote").innerHTML = can_vote + " to vote.";
+}
+
+// NEW AND THIS KEYWORDS
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make;
+    this.Vehicle_Model;
+    this.Vehicle_Year;
+    this.Vehicle_Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erick drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + "manufactured in  " + Erik.Vehicle_Year;
+}
+
+//TESTING IN VALID VALUES FOR VARIABLES 
+function test1() {
+    var abstract = 2;
+    window.alert(abstract)
+}
+
+// OBJECT CONSTRUCTOR FUNCTION 
+function Phone(Screen_size, Resolution, CPU, RAM,) {
+    this.Screen_size;
+    this.Resolution;
+    this.CPU;
+    this.RAM
+}
+var Samsung_Galaxy_S10 = new Phone(6.1, 1440, "Snapdragon855", "8 gigabytes")
+function object_mine() {
+    document.getElementById("New_and_This").innerHTML = "The Samsung Galaxy S10 has a " + Samsung_Galaxy_S10.Screen_size + "the resolution of the screen is " + Samsung_Galaxy_S10.Resolution + "the CPU is a " + Samsung_Galaxy_S10.CPU + "last but not least it has " + Samsung_Galaxy_S10.RAM;
+}
+
+//NESTED FUNCTIONS 
+function count_Function() {
+    document.getElementById("Nested_function").innerHTML = Count();
+    function Count() {
+        var Starting_point = 20;
+        function Plus_6() {Starting_point += 6;} {
+            Plus_6();
+            return Starting_point;
+        }
+    }
+}
+
