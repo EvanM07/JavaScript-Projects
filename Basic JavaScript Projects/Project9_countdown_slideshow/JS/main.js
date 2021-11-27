afeea0513dc8fd0d4cb9924ed0a8717bc16df1c4
Evan Masters <92110978+EvanM07@users.noreply.github.com>
2021-11-26 21:13:27 -0800

    added some code
    added a counting program

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -10,3 +10,14 @@ function countdown() {
         }
         tick();
 }
+
+// COUNTING TO TEN. WHILE LOOP
+function count_To_Ten() {
+    var Digit = "";
+    var X = 1;
+    while(X < 11){
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+}
